5628ab29534777d965afec1d9b52808d606894c7
lalit <lkjlalitkumar2005@gmail.com>
2025-03-27 19:48:43 +0530

    Production

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -1,7 +1,16 @@
+
 export const authMiddleware = (req, res, next) => {
-    if (req.isAuthenticated()) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  return res.status(401).json({ error: "Not authenticated" });
+};
+
+export const roleMiddleware = (role) => {
+  return (req, res, next) => {
+    if (req.isAuthenticated() && req.user.role === role) {
       return next();
     }
-    res.status(401).json({ message: "Unauthorized. Please log in." });
+    return res.status(403).json({ error: "Insufficient permissions" });
   };
-  
+};
