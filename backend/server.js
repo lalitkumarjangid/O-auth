77ff5727858d29b610b3169094d0cfcf1b9095d3
lalit <lkjlalitkumar2005@gmail.com>
2025-03-27 21:26:01 +0530

    Added Cors

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,8 @@ app.use(
       ? process.env.FRONTEND_URL 
       : "http://localhost:5173",
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   })
 );
 app.use(express.json());
